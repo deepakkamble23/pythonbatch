fd331519dc4cb53495238157c70e129ca02609a2
deepakkamble23 <156627233+deepakkamble23@users.noreply.github.com>
2024-01-18 10:52:46 +0530

    Add files via upload

---
+++ studentscore.py
@@ -0,0 +1,11 @@
+score=int(input("Enter students marks :"))
+if score>=75 and score<75:
+    print("Disinction")
+elif score>=60 and score<75:
+    print("First Class")
+elif score>=45 and score<60:
+    print(" Second Class")
+elif score<40:
+    print("Fail")
+else:
+    print("Enter valid number")
